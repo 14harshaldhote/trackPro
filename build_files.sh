582e51c373f289bdc9941e085771049bcdf331de
Harshal Dhote <dhoteh020@gmail.com>
2025-12-07 15:43:53 +0530

    prod test 2

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,5 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Download spacy model (optional, for NLP features)
-python -m spacy download en_core_web_sm || true
-
 # Collect static files
 python manage.py collectstatic --noinput
